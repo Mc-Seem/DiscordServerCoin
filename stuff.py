ac9fc0cdc3d0f9d78cd68fd57dd9a4752eee315a
Mc-Seem <applejack188288@gmail.com>
2020-01-26 16:12:28 +0300

    Added trade and shop introductions

--- stuff.py
+++ stuff.py
@@ -8,5 +8,13 @@ wrong_channel = f"You are in public chat. " \
 trade_init_msg = 'This channel has successfully been set to be a trade channel'
 shop_init_msg = 'This channel has successfully been set to be a shop channel'
 no_permission = 'You have not enough permissions to do that'
-trade_introduction = ''
-shop_introduction = ''
+trade_introduction = 'This is a trade room for the items. ' \
+                     '\nYou can sell an item by using a command "trade-sell".' \
+                     '\nYou can bid for an item by using a command "trade-bid <DSC amount>"' \
+                     '\nIf your bid was the highest when the time had expired, a group chat with both sides' \
+                     ' will be opened' \
+                     '\n When you get an item, you can confirm your deal by using a command "confirm"' \
+                     '\n We wish you a good trade!'
+shop_introduction = "This is a server's dedicated shop." \
+                    "\n You can sell an item if you are the administrator by using a command \"shop-sell\""
+# ToDo: Сделать возможность продавать роли или еще что-то, то есть выделить функции под это.
